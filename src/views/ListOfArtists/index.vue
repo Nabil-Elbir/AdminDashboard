cc3d114ea1b43ec15f0c5fe1f71d91356612536a
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-19 00:19:50 +0100

    init

---
+++ src/views/ListOfArtists/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app-container">
+    <el-table
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="ID" width="50" align="center">
+        <template>
+          ID
+        </template>
+      </el-table-column>
+      <el-table-column label="Name" width="140" align="center">
+        <template>
+          Name
+        </template>
+      </el-table-column>
+      <el-table-column label="#" width="300" align="center">
+        <template>
+          #
+        </template>
+      </el-table-column>
+      <el-table-column label="#" width="100" align="center">
+        <template>
+          #
+        </template>
+      </el-table-column>
+      <el-table-column label="#" width="300" align="center">
+        <template>
+          #
+        </template>
+      </el-table-column>
+      <el-table-column label="#" align="center">
+        <template>
+          #
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
---
+++ src/views/ListOfCategories/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app-container">
+    <el-table
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="ID" width="50" align="center">
+        <template>
+          ID
+        </template>
+      </el-table-column>
+      <el-table-column label="Name" width="140" align="center">
+        <template>
+          Name
+        </template>
+      </el-table-column>
+      <el-table-column label="Description" width="300" align="center">
+        <template>
+          Description
+        </template>
+      </el-table-column>
+      <el-table-column label="Artists" width="100" align="center">
+        <template>
+          Artists
+        </template>
+      </el-table-column>
+      <el-table-column label="Image URL" width="300" align="center">
+        <template>
+          Image URL
+        </template>
+      </el-table-column>
+      <el-table-column label="Controls" align="center">
+        <template>
+          Controls
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
